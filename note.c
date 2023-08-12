ee5bbfa1a6e34fda9dadc8f9e5bdce19fa318abe
abdelhadia72 <thisisavnb@gmail.com>
2023-08-12 15:29:06 +0100

    start thinking

---
+++ note.c
@@ -0,0 +1,12 @@
+// Notes //
+//! FUNCTIONS
+//& spliter : split a string into array
+//& runcmd : rum only the commend and return 0 or -1
+//&
+
+
+
+//? Function to add .
+// strcmp
+// strtok
+// strdup

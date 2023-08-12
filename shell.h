a2198d6b8b7158143373002e5b14f9d9cba30dcc
abdelhay <abdelhay@gmail.com>
2023-08-12 03:29:20 -0700

    task 0 v_0

--- shell.h
+++ shell.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define MAX_INPUT_LENGTH 1024
 
--- simple_shell.c
+++ simple_shell.c
@@ -3,11 +3,45 @@
 int main()
 {
 	char input[MAX_INPUT_LENGTH];
+	int status;
 
 	while (1)
 	{
+		// display the promet 
 		printf("#cisfun$ ");
 		fflush(stdout);
+
+		// read user input
+		if (fgets(input, sizeof(input), stdin) == NULL)
+		{
+			// handle Ctrl+D
+			break;
+		}
+
+		// execute command using execve
+		pid_t child_pid = fork();
+
+		if (child_pid == -1)
+		{
+			perror("error");
+			exit(1);
+		}
+		else if (child_pid == 0)
+		{
+			if (strlen(input) > 0)
+			{
+				if (execlp(input, input, NULL) == -1)
+				{
+					perror("error");
+					exit(1);
+				}
+			}
+			exit(0);
+		}
+		else
+		{
+			waitpid(child_pid, &status, 0);
+		}
 	}
 	return 0;
 }

533239a6090e9eb096f3b8b0343900ae4d2ebdfd
abdelhay <abdelhay@gmail.com>
2023-08-12 03:07:06 -0700

    task 0 strat working on it

---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAX_INPUT_LENGTH 1024
+
+#endif
---
+++ simple_shell.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+int main()
+{
+	char input[MAX_INPUT_LENGTH];
+
+	while (1)
+	{
+		printf("#cisfun$ ");
+		fflush(stdout);
+	}
+	return 0;
+}

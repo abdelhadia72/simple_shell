19effcfcb8e609e8d7bccad2ad7494abf9abd5e6
abdelhay <abdelhay@gmail.com>
2023-08-16 02:35:59 -0700

    kl

--- shell.c
+++ shell.c
@@ -1,55 +1,67 @@
 #include "shell.h"
 
+/**
+ * main - Entry point for the custom shell program.
+ *
+ * @ac: Number of command-line arguments.
+ * @av: Array of command-line argument strings.
+ *
+ * Return: Always returns 0.
+ */
 
 int main(int ac, char **av)
 {
-    char *buffer = NULL;
-    char **args = {NULL};
-    size_t size = 0;
-    
-    if (isatty(0)) {
-        _puts("$ ");
-    }
-    while(getline(&buffer, &size, stdin) != EOF)
-    {
-        buffer[strlen(buffer) - 1] = '\0';
-        
-        if(strcmp(buffer, "exit") == 0)
-        {
-            free(buffer);
-            perror(buffer);
-            exit(98);
-            break;
-        }
-        
-        // check on spaces
-        if(!space_check(buffer))
-        {
-            args = spliter(buffer);
-            
-            if (strcmp(args[0], "cd") == 0) {
-                change_dir(args);
-            }else  if (strcmp(buffer, "echo $?") == 0)
-            {
-                
-                printf("exit %d", exit_status);
-            }else  if (strcmp(buffer, "echo $$") == 0)
-            {
-                printf("%d\n",getppid());
-            }
-            else
-            {
-                runcmd(args);
-            }
-        }else
-        {
-            // printf("%s\n", buffer);
-        }
-        
-        if (isatty(0)) {
-            _puts("$ ");
-        }
-    }
+	char *buffer = NULL;
+	char **args = {NULL};
+	size_t size = 0;
+	
+	if (isatty(0))
+	{
+		_puts("$ ");
+	}
+	while(getline(&buffer, &size, stdin) != EOF)
+	{
+		buffer[strlen(buffer) - 1] = '\0';
+		
+		if(strcmp(buffer, "exit") == 0)
+		{
+			free(buffer);
+			perror(buffer);
+			exit(98);
+			break;
+		}
+		
+		// check on spaces
+		if(!space_check(buffer))
+		{
+			args = spliter(buffer);
+			
+			if (strcmp(args[0], "cd") == 0)
+			{
+				change_dir(args);
+			}
+			else if(strcmp(buffer, "echo $?") == 0)
+			{
+				printf("exit %d", exit_status);
+			}
+			else if (strcmp(buffer, "echo $$") == 0)
+			{
+				printf("%d\n",getppid());
+			}
+			else
+			{
+				runcmd(args);
+			}
+		}else
+		{
+			// printf("%s\n", buffer);
+		}
+		
+		if (isatty(0))
+		{
+			_puts("$ ");
+		}
+	}
 }
 
 

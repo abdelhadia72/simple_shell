3c011595f68448b9345677905d21c9f003fe5b94
abdelhay <abdelhay@gmail.com>
2023-08-16 11:24:07 -0700

    create atoi

--- function.c
+++ function.c
@@ -16,3 +16,25 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+/* make the atoi */
+
+int _atoi(char *nptr)
+{
+	int i = 0;
+	int n = 0;
+
+	if (nptr[i] == '+')
+		i++;
+
+	while (nptr[i])
+	{
+		if (nptr[i] < '0' || nptr[i] > '9')
+			return (-1);
+
+		n = (n * 10) + (nptr[i] - '0');
+		i++;
+	}
+
+	return (n);
+}

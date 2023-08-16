20cae31f5e73a6cbd4f27e59b0bb5a88f2d169f1
abdelhadia72 <thisisavnb@gmail.com>
2023-08-16 21:48:13 +0100

    fix cd -

--- change_dir.c
+++ change_dir.c
@@ -6,19 +6,30 @@ void change_dir(char **args)
     char oldpwd[100];
     char *path = args[1];
     
-    getcwd(oldpwd, sizeof(oldpwd));
-    setenv("OLDPWD", oldpwd, 1);
+    // getcwd(oldpwd, sizeof(oldpwd));
+    // setenv("OLDPWD", oldpwd, 1);
+    // printf("old path is -> %s\n", getenv("OLDPWD"));
+    // printf("before %s\n", oldpwd);
     
-    printf("oldpwd %s\n", getenv("OLDPWD"));
+    // printf("oldpwd %s\n", getenv("OLDPWD"));
     if(strcmp(path, "-") == 0)
     {
         //! fix me
+
+        
         printf("changed to the preiv dir\n");
         if (chdir(getenv("OLDPWD")) == -1) {
             perror("cd");
         }
     } else
     {
+    
+        printf("-----------------\n");
+        getcwd(oldpwd, sizeof(oldpwd));
+        setenv("OLDPWD", oldpwd, 1);
+        printf("old path is -> %s\n", getenv("OLDPWD"));
+        printf("after %s\n", oldpwd);
+    
         if (path == NULL) {
             exit(FAILURE);
         } else {

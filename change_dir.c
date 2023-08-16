5b7e14818ef3f191af5f392aa8f9225e3f50f25e
abdelhadia72 <thisisavnb@gmail.com>
2023-08-16 21:52:04 +0100

    handle cd ~

--- change_dir.c
+++ change_dir.c
@@ -5,31 +5,24 @@ void change_dir(char **args)
     int i = 0;
     char oldpwd[100];
     char *path = args[1];
-    
-    // getcwd(oldpwd, sizeof(oldpwd));
-    // setenv("OLDPWD", oldpwd, 1);
-    // printf("old path is -> %s\n", getenv("OLDPWD"));
-    // printf("before %s\n", oldpwd);
-    
-    // printf("oldpwd %s\n", getenv("OLDPWD"));
+
     if(strcmp(path, "-") == 0)
     {
         //! fix me
-
-        
-        printf("changed to the preiv dir\n");
         if (chdir(getenv("OLDPWD")) == -1) {
             perror("cd");
         }
-    } else
+    }else if(strcmp(path, "~") == 0)
+    {
+        if (chdir(getenv("HOME")) == -1) {
+            perror("cd");
+        }
+    }
+    else
     {
     
-        printf("-----------------\n");
         getcwd(oldpwd, sizeof(oldpwd));
         setenv("OLDPWD", oldpwd, 1);
-        printf("old path is -> %s\n", getenv("OLDPWD"));
-        printf("after %s\n", oldpwd);
-    
         if (path == NULL) {
             exit(FAILURE);
         } else {
@@ -49,8 +42,3 @@ void change_dir(char **args)
     free(args);
     
 }
-
-// get the current directory
-// set it the the old_pwd 
-// then change the directory
-//! remove the repat of the code 

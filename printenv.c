57caab9848ae3b25c9b9c1235f9da957b2502e91
abdelhadia72 <thisisavnb@gmail.com>
2023-08-23 20:15:08 +0100

    fix env3

--- printenv.c
+++ printenv.c
@@ -3,15 +3,10 @@
 void printenv()
 {
     extern char **environ;
-    int envIndex;
-    char newlineChar = '\n';
+    char **env;
 
-    if (!environ)
-        exit(1);
-
-    for (envIndex = 0; environ[envIndex]; envIndex++)
+    for (env = environ; *env != NULL; env++)
     {
-        write(STDOUT_FILENO, environ[envIndex], _strlen(environ[envIndex]));
-        write(STDOUT_FILENO, &newlineChar, 1);
+        printf("%s\n", *env);
     }
 }

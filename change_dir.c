aea9ef6a49745321471833d5d7f13de9acd408f5
abdelhadia72 <thisisavnb@gmail.com>
2023-08-23 15:28:59 +0100

    cd fixxx

--- change_dir.c
+++ change_dir.c
@@ -2,22 +2,12 @@
 
 void change_dir(char **args)
 {
-    int i = 0;
-    char oldpwd[100];
-    char *path = args[1];
+    char oldpwd[265];
+    int i;
 
-    if (path == NULL || strcmp(path, "~") == 0)
+    if (!args[1] || strcmp(args[1], "~") == 0)
     {
-        /* Get the home directory path */
-        const char *home_path;
-        if ((home_path = getenv("HOME")) == NULL)
-        {
-            struct passwd *pw = getpwuid(getuid());
-            if (pw)
-            {
-                home_path = pw->pw_dir;
-            }
-        }
+        const char *home_path = getenv("HOME");
 
         if (home_path)
         {
@@ -31,28 +21,35 @@ void change_dir(char **args)
             fprintf(stderr, "cd: could not determine home directory\n");
         }
     }
-    else if (strcmp(path, "-") == 0)
+    else if (strcmp(args[1], "-") == 0)
     {
-        if (chdir(getenv("OLDPWD")) == -1)
+        const char *old_path = getenv("OLDPWD");
+
+        if (old_path && chdir(old_path) == -1)
         {
             perror("cd");
         }
     }
     else
     {
-        getcwd(oldpwd, sizeof(oldpwd));
-        setenv("OLDPWD", oldpwd, 1);
-        if (chdir(path) == -1)
+        if (getcwd(oldpwd, sizeof(oldpwd)) == NULL)
+        {
+            perror("getcwd");
+        }
+        else
+        {
+            setenv("OLDPWD", oldpwd, 1);
+        }
+
+        if (chdir(args[1]) == -1)
         {
             perror("cd");
         }
     }
 
-    /* Free memory */
-    while (args[i])
+    for (i = 0; args[i]; ++i)
     {
         free(args[i]);
-        i++;
     }
     free(args);
 }

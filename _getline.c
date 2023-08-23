a2545ed0ee13736652cadb2a3848b0cc75275981
abdelhadia72 <thisisavnb@gmail.com>
2023-08-23 23:24:08 +0100

    env fixing..

--- _getline.c
+++ _getline.c
@@ -2,44 +2,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *_getline(char **buffer, size_t *buffer_size, FILE *stream) {
-    size_t buffer_index = 0;
-    size_t chars_in_buffer = 0;
-    char current_char;
+int *_getline(char **buffer, size_t *buffer_size, FILE *stream)
+{
+	size_t buffer_index = 0;
+	size_t chars_in_buffer = 0;
+	char current_char;
 
-    char *line = NULL;
-    size_t line_length = 0;
+	char *line = NULL;
+	size_t line_length = 0;
 
-    *buffer = (char *)malloc(*buffer_size * sizeof(char));
+	*buffer = (char *)malloc(*buffer_size * sizeof(char));
 
-    while (1) {
-        if (buffer_index >= chars_in_buffer) {
-            chars_in_buffer = fread(*buffer, sizeof(char), *buffer_size, stream);
-            buffer_index = 0;
+	while (1)
+	{
+		if (buffer_index >= chars_in_buffer)
+		{
+			chars_in_buffer = fread(*buffer, sizeof(char), *buffer_size, stream);
+			buffer_index = 0;
 
-            if (chars_in_buffer == 0) {
-                if (line_length == 0) {
-                    free(*buffer);
-                    return NULL;
-                } else {
-                    break;
-                }
-            }
-        }
+			if (chars_in_buffer == 0)
+			{
+				if (line_length == 0)
+				{
+					free(*buffer);
+					return NULL;
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
 
-        current_char = (*buffer)[buffer_index++];
-        if (current_char == '\n') {
-            break;
-        }
+		current_char = (*buffer)[buffer_index++];
+		if (current_char == '\n')
+		{
+			break;
+		}
 
-        line = realloc(line, line_length + 1);
-        line[line_length++] = current_char;
-    }
+		line = realloc(line, line_length + 1);
+		line[line_length++] = current_char;
+	}
 
-    if (line != NULL) {
-        line = realloc(line, line_length + 1);
-        line[line_length] = '\0';
-    }
+	if (line != NULL)
+	{
+		line = realloc(line, line_length + 1);
+		line[line_length] = '\0';
+	}
 
-    return line;
+	return line;
 }

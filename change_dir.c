7b9a7ea300a3b39b6c946007e52e5e7a6c738ac0
abdelhay <abdelhay@gmail.com>
2023-08-23 18:35:15 -0700

    betty change dir

--- change_dir.c
+++ change_dir.c
@@ -19,39 +19,26 @@ void change_dir(char **args)
 		if (home_path)
 		{
 			if (chdir(home_path) == -1)
-			{
 				perror("cd");
-			}
 		}
 		else
-		{
 			_puts("cd: could not determine home directory");
-		}
 	}
 	else if (strcmp(args[1], "-") == 0)
 	{
 		const char *old_path = getenv("OLDPWD");
 
 		if (old_path && chdir(old_path) == -1)
-		{
 			perror("cd");
-		}
 	}
 	else
 	{
 		if (getcwd(oldpwd, sizeof(oldpwd)) == NULL)
-		{
 			perror("getcwd");
-		}
 		else
-		{
 			setenv("OLDPWD", oldpwd, 1);
-		}
-
 		if (chdir(args[1]) == -1)
-		{
 			perror("cd");
-		}
 	}
 
 	for (i = 0; args[i]; ++i)

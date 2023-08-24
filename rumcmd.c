c9f0557ec06b10781a05e2d3d3abbdd918540c14
abdelhay <abdelhay@gmail.com>
2023-08-23 18:26:54 -0700

    betty rumcmd

--- rumcmd.c
+++ rumcmd.c
@@ -8,48 +8,42 @@
  */
 void runcmd(char **args)
 {
-    char *path = NULL;
-    pid_t pid = fork();
-    int i = 0;
-    int stat;
+	char *path = NULL;
+	pid_t pid = fork();
+	int i = 0;
+	int stat;
 
-    if (pid == -1)
-    {
-        perror("fork");
-        exit(1);
-    }
-    else if (pid == 0)
-    {
-        if (args[0][0] == '/')
-        {
-            path = args[0];
-        }
-        else
-        {
-            path = which(args[0]);
-            if (!path)
-            {
-                fprintf(stderr, "Command not found: %s\n", args[0]);
-                exit(1);
-            }
-        }
-
-        if (execve(path, args, environ) == -1)
-        {
-            perror("execve");
-            free(path);
-            exit(2);
-        }
-    }
-    else
-    {
-        waitpid(pid, &stat, 0);
-    }
-
-    while (args[i])
-    {
-        free(args[i]);
-        i++;
-    }
-    free(args);
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(1);
+	}
+	else if (pid == 0)
+	{
+		if (args[0][0] == '/')
+			path = args[0];
+		else
+		{
+			path = which(args[0]);
+			if (!path)
+			{
+				fprintf(stderr, "Command not found: %s\n", args[0]);
+				exit(1);
+			}
+		}
+		if (execve(path, args, environ) == -1)
+		{
+			perror("execve");
+			free(path);
+			exit(2);
+		}
+	}
+	else
+		waitpid(pid, &stat, 0);
+	while (args[i])
+	{
+		free(args[i]);
+		i++;
+	}
+	free(args);
 }
